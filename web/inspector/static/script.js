3b24baf208d7a1cb4f2d3c45bf6b0a10ffcecea5
anir-ab <anibhagwat17@gmail.com>
2023-03-25 15:43:47 +0530

    updated inspector

--- web/inspector/static/script.js
+++ web/inspector/static/script.js
@@ -1,8 +1,33 @@
-document.querySelector('h1').addEventListener('click', function() {
-    this.style.color = 'red';
-  });
-// _CTF}
-  document.querySelector('p').addEventListener('mouseover', function() {
-    this.style.fontSize = '20px';
-  });
-  
+(async () => {
+  await new Promise((res) => window.addEventListener('load', res))
+
+  const state = {
+      velocity: 0,
+      angle: 0,
+      time: undefined,
+  }
+
+  const element = document.querySelector('img')
+  const animate = (current) => {
+      if (state.time === undefined) state.time = current
+
+      const delta = current - state.time
+      state.angle = (state.angle + state.velocity * delta / 1000) % 360
+
+      element.style.transform = `rotate(${state.angle}deg)`
+
+      state.time = current
+      requestAnimationFrame(animate)
+  }
+  requestAnimationFrame(animate)
+
+  window.addEventListener('click', () => {
+      state.velocity += 10;
+  })
+
+  setInterval(() => {
+      state.velocity = Math.max(0, state.velocity - 1)
+  }, 100)
+})()
+
+// flag part 3: EvErywHEr3}
